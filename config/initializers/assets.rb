9bdd5897b73272a2b4f1340b6cba4b2b89eefeed
petr.nikolas.prokop <petrnikolasprokop@yahoo.com>
2016-09-14 13:58:28 +0200

    update project

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,4 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( application.scss home.js home.scss posts.js posts.scss portfolio.scss portfolio.js )
+Rails.application.config.assets.precompile += %w( application.scss home.js home.scss posts.js posts.scss )
